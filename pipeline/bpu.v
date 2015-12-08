23ad889db8837ac26a86c70819bb42a92c1393d1
Wonicon <conciselove@outlook.com>
2015-12-09 02:17:34 +0800

    调整代码结构

--- pipeline/bpu.v
+++ pipeline/bpu.v
@@ -19,12 +19,6 @@
 // 为了减少延迟槽的 nop 指令带来的性能损失, 顺序执行地址存储的是 PC + 8
 // 由于标签的位宽有限, 所以会有碰撞的情况出现, 在 MEM 段修正后 PC + 4 也可以存储到里面
 //
-// 表项格式
-// +------------------------------+------+------+
-// |       预测地址               |预测位|有效位|
-// +------------------------------+------+------+
-//          [31:2]                   [1]    [0]
-//
 
 // 标签位宽
 `define TAG_WIDTH 6
@@ -35,72 +29,75 @@
 // 预测表条目数
 `define NR_SLOT (2 ** `TAG_WIDTH)
 
-// 总线截取
-`define PC_BUS      (`PC_WIDTH - 1)    : 0
-`define ENTRY_BUS   (`ENTRY_WIDTH - 1) : 0
-`define TABLE_BUS   (`NR_SLOT - 1)     : 0
-`define PREDICT_BUS (`PC_WIDTH - 1)    : 2
-`define TAG_BUS 7:2
-
-// 状态位助记符
-`define VALID 0
-`define PREDICT 1
+// 总线
+`define PC_BUS       (`PC_WIDTH - 1)  : 0
+`define VALID_PC_BUS (`PC_WIDTH - 3)  : 0
+`define VALID_SLICE  (`PC_WIDTH - 1)  : 2
+`define INDEX_BUS    (`NR_SLOT - 1)   : 0
+`define TAG_BUS      (`TAG_WIDTH - 1) : 0
 
 module bpu(
     input clk,
     input reset,
+    input bpu_w_en,                    // 写使能, 驱动 bpu 进行状态更新
     input [`PC_BUS] current_pc,        // 当前用来查询下一条指令的PC
     input [`PC_BUS] tag_pc,            // 用来获取标签的PC
     input [`PC_BUS] next_pc,           // 标签PC对应的下一条执行指令的PC
-    input bpu_w_en,                    // 写使能, 驱动 bpu 进行状态更新
     output reg [`PC_BUS] predicted_pc  // 预测 PC
 );
 
-reg [`ENTRY_BUS] bpu_table [`TABLE_BUS];
+reg [`VALID_PC_BUS] bpu_pc [`INDEX_BUS]; // 存储预测pc
+reg bpu_valid [`INDEX_BUS];            // 存储有效位, 指示是否使用预测pc
+reg bpu_predict [`INDEX_BUS];          // 存储预测位, 指示预测pc是否可以修改
 
 integer i;
 initial begin
     for (i = 0; i < `NR_SLOT; i = i + 1) begin
-        bpu_table[i] = 0;
+        bpu_pc[i] = 0;
+        bpu_valid[i] = 0;
+        bpu_predict[i] = 0;
     end
 end
 
+wire [`VALID_PC_BUS] valid_current_pc = current_pc[`VALID_SLICE];
+wire [`VALID_PC_BUS] valid_tag_pc = tag_pc[`VALID_SLICE];
+
 // 预测逻辑
-wire [`TAG_WIDTH - 1 : 0] predict_tag = current_pc[`TAG_BUS];
-wire [`ENTRY_BUS] predict_slot = bpu_table[predict_tag];
+wire [`TAG_BUS] predict_tag = valid_current_pc[`TAG_BUS];
 
 always @(*) begin
-   if (predict_slot[`VALID] == 1'b1) begin
-       predicted_pc = { predict_slot[`PREDICT_BUS], 2'b0 };
+   if (bpu_valid[predict_tag] == 1'b1) begin
+       predicted_pc = { bpu_pc[predict_tag], 2'd0 };
    end
    else begin
-       predicted_pc = current_pc + 4;
+       predicted_pc = { valid_current_pc + 1, 2'd0 };
    end
 end
 
 // 更新逻辑
-wire [`TAG_WIDTH - 1 : 0] update_tag = tag_pc[`TAG_BUS];
-wire [`ENTRY_BUS] update_slot = bpu_table[update_tag];
-
+wire [`TAG_BUS] update_tag = tag_pc[`TAG_BUS];
+wire update_predict = bpu_predict[update_tag];
 always @(negedge clk or posedge reset) begin
     if (reset) begin
         for (i = 0; i < `NR_SLOT; i = i + 1) begin
-            bpu_table[i] <= 0;
+            bpu_pc[i] <= 0;
+            bpu_valid[i] <= 0;
+            bpu_predict[i] <= 0;
         end
     end
     else if (bpu_w_en) begin
         // 有效位有效后一直有效
-        bpu_table[update_tag][`VALID] <= 1'b1;
+        bpu_valid[update_tag] <= 1'b1;
         // 预测位为 0 时立即更新, 之后容忍一次错误
-        if (update_slot[`PREDICT] == 1'b0) begin
-            bpu_table[update_tag][`PREDICT_BUS] <= next_pc[`PREDICT_BUS];
+        if (update_predict == 1'b0) begin
+            bpu_pc[update_tag] <= next_pc[`VALID_SLICE];
         end
         // 翻转预测位, 因为只有一位, 相当于加一
-        bpu_table[update_tag][`PREDICT] <= ~update_slot[`PREDICT];
+        bpu_predict[update_tag] <= ~update_predict;
     end
     else begin
         // 预测正确的情况, 将预测位维持在 1
-        bpu_table[update_tag][`PREDICT] <= 1'b1;
+        bpu_predict[update_tag] <= 1'b1;
     end
 end
 
